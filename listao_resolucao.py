1e713d5407000e1232f19f7db94de33c600603b6
Lya <lyacarolina94@gmail.com>
2022-09-04 20:37:33 -0300

    Somente exerc 12

--- listao_resolucao.py
+++ listao_resolucao.py
@@ -1,9 +1,8 @@
-#exerc 10
+#exerc 12
 
-nome = input("Insira seu nome: ")
-cidade = input("Insira a cidade onde nasceu: ")
-ano_nasc = input("Insira o ano que nasceu: ")
-print(nome+'\n'+cidade+'\n'+ano_nasc)
-x = int(ano_nasc)
-idade = 2030 - x
-print("Em 2030 você terá {} anos.".format(idade))
+b = input("Informe o tamanho da base: ")
+h = input("Informe o tamanho da altura: ")
+x = float(b)
+y = float(h)
+area = (x*y)/2
+print("A área é de {} unidades de medida²".format(area))
